6ae02faf832880239ff756122a0cadea0f75cb16
unknown <theufesa@gmail.com>
2021-05-17 10:58:13 +0300

    add file

---
+++ lesson_2.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<cstdlib>
+#include<cstring>
+
+using namespaces std;
+
+int t = 17;
+
+extern int x;
+static int y;
+
+int main{
+
+	x = 21;
+	y = 37;
+
+
+	char number = 27;
+	float c = 7.7;
+	double h = 1.33434534;
+	bool b = false;
+
+
+	enum game x_0;
+	{
+		
+	};
+}
+
