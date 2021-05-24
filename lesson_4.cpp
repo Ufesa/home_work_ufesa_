2cc0771080e2e1512f8e2e81d0856eb2f97e6c9f
ufesa <theufesa@gmail.com>
2021-05-24 20:47:50 +0300

    added homework

--- lesson_4.cpp
+++ lesson_4.cpp
@@ -2,10 +2,15 @@
 #include<cstring>
 #include<cstdlib>
 
+const int c = 10;
+const int j = 5;
+
 
 int main(){
 
 	using namespace std;
+
+//task 1;
 
 	 int a;
 	 int b;
@@ -20,14 +25,14 @@ int main(){
     result = a + b;
 
     	if (result >= 10 && result <=20) {
-    		cout <<"Result of a + b = true "<<result<<"\n";
+    		cout <<"Result of a + b = true - "<<result<<"\n";
     	} else {
-    		cout <<"Result of a + b = false (not in this aread of 10 to 20) "<<result<<"\n";	
+    		cout <<"Result of a + b = false (not in this aread of 10 to 20) -  "<<result<<"\n";	
     	}
     	
     
 //2 task:
-    	cout<<"Enter number = "<<"\n";
+    	/*/cout<<"Enter number = "<<"\n";
     	cin>>a;
 
     	float result_2;
@@ -41,28 +46,61 @@ int main(){
     		cout <<result_2<<"\n";
     	} else {
     		cout<<"Variable isn't simple number = "<<result_2_0<<"\n";
-    	}
+    		}*/
 
 
 
+//tasks 3; 
 
+cout<<"Task 3: "<<"\n";
 
-
-//Set massive 3x3x3
-
-int array [3] [3] [3];
-int val =0;
-
-for (int i =0; i < 3; i++){
-    
-    for (int p =0; p < 3; p++){
-        
-    for (int t =0; t < 3; t++)
-        array [i][p][t] = val++;
-    }
+if (c == 10 && j == 10) {
+	return true;
 }
 
-cout<<"Element - [][][] = "<<*(*(*(array + 2) +2) +2)<<"\n";
+if ((c + j) == 10) {
+	return true;
+}
+	cout<<"c + j != 10 -- "<< c + j<<"\n";
+	
+
+//task 4;
+
+//Set massive 2x2
+
+
+int array [c][j];
+int val = 0;
+int val_1 = 1;
+
+for (int i =0; i < c; ++i){
+    for (int p =0; p < j; ++p){
+    	if( i == p || (i + p +1)) { 
+    		array [i][p] = val_1;
+        }
+        array [i][p] = val;
+	}
+}
+
+//read out array;
+for (int i = 0; i < c; ++i){
+	for(int p = 0; p < j; ++p){
+	cout<<array [i][p]<<" "<<"\n";
+	}
+}
+
+//task 5;
+
+int Year = 0;
+
+cout<<"Enter year: ";
+
+cin>>Year;
+	if ((Year % 4 == 0) && (Year % 100 != 0) || (Year % 400 == 0)) {
+		cout<<"This is High year"<<"\n";
+	}
+	
+	cout<<"This year isn't high ^)"<<"\n";
 
 	return 0;
 }
