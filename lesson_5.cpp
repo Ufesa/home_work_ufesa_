936507155c9d07ed71f33411d9cb0348b9de87ed
ufesa <theufesa@gmail.com>
2021-05-26 21:32:23 +0300

    added new task to lesson_5

--- lesson_5.cpp
+++ lesson_5.cpp
@@ -6,11 +6,14 @@ using namespace std;
 
 int map[10] = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
 int p = 1;
-int masive [4] = {1,2,2,4};
+int masive[4] = {1,2,2,4};
+int numbers[8] = {1,3,5,6,7,8,10,12};
 
 void swap ();
-int add3(int );
+int add3 (int );
 void masiveCheck (int masive[], int n);
+void swapElements (int numbers[], int p, int n);
+
 
 int main(){
 
@@ -35,6 +38,9 @@ int main(){
          
          //task 3
          masiveCheck(masive, 4);
+         
+         //task4l;
+         swapElements(numbers, 8, 2);
          
          
 //tasks 2; 
@@ -71,11 +77,32 @@ int add3(int ) {
 }
 
 void masiveCheck(int masive[], int n) {
-	for(int i = 0; i < 4; ++i){
+	for(int i = 0; i < n; ++i){
 		if (masive[i] + masive[i+1] == masive[i+2] + masive[i+3]){
 		cout<<"sum of both sides of masive is equal"<<endl;
 		break;
 	} cout<<"sum of both sides  of masive is !="<<endl;
 	break;
         }
+}
+void swapElements( int numbers[], int p, int n){
+	cout<<"numbers: ";
+	for(int i = 0; i < p; ++i) { 
+	cout<<numbers[i]<<" ";
+	}
+	cout<<endl;
+	cout<<"number n: "<<n<<"\n";
+// shift elements in the numbers arrayl;
+    int tmp = numbers[0];
+    for(int i = 0; i < p-n; ++i) { 
+        numbers[i] = numbers[i + 1];
+        if(i == p-n)
+            numbers[i] =tmp;
+    }
+    
+    cout<<"swaped numbers: ";
+	for(int i = 0; i < p; ++i) { 
+	cout<<numbers[i]<<" ";
+	}
+	cout<<endl;
 }
