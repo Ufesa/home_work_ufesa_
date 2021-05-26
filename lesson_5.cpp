46d73b15db8c54a9e93e06b3438063e03d1b43a1
ufesa <theufesa@gmail.com>
2021-05-26 18:23:33 +0300

    added few tasks to lesson_5

--- lesson_5.cpp
+++ lesson_5.cpp
@@ -4,21 +4,11 @@
 
 using namespace std;
 
-int *map[10] = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
-
-void swap ();
-
-int main(){
-
-//task 1;#include<iostream>
-#include<cstring>
-#include<cstdlib>
-
-using namespace std;
-
 int map[10] = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+int p = 1;
 
 void swap ();
+int add3(int );
 
 int main(){
 
@@ -38,14 +28,20 @@ int main(){
          for (int i = 0; i < 9; i++) {
         cout<<map[i]<<" ";
          }
+         cout<<endl;
 //tasks 2; 
-    
 
-//task 4;
-
-//Set massive 2x2
-
-//task 5;
+int array [8];
+//fill up array;
+    for (int i =0; i < 8; ++i) {
+        array[i] = p;
+        add3(p); 
+    }
+    cout<<"array :"<<"\n";
+    for (int i =0; i < 8; ++i) {
+        cout<<array[i]<<" "; 
+    }
+    cout<<endl;
 
     return 0;
 }
@@ -55,31 +51,7 @@ void swap() {
         int map[10] = {0, 0, 1, 1, 0, 1, 0, 0, 1, 1};
 }
 
-    //show map;
-    
-    for (int i = 0; i < 9; i++ ) {
-        cout<<map[i]<<" ";
-    }
-    // swap();
-
-
-//tasks 3; 
-    
-
-//task 4;
-
-//Set massive 2x2
-
-//task 5;
-
-    return 0;
-}
-
-void swap() {
-    //swap values;
-        map[10] = {0, 0, 1, 1, 0, 1, 0, 0, 1, 1};
-
-     for (int i = 0; i < 9; p++) {
-        cout<<map[i]<<" ";
-    }
+int add3(int ){
+    p = p + 3;
+    return p;
 }
