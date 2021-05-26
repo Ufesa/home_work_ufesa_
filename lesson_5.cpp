0d25aff3e1333dab1249e5d97cbf946608ac8231
ufesa <theufesa@gmail.com>
2021-05-26 21:04:03 +0300

    added new task to lesson_5

--- lesson_5.cpp
+++ lesson_5.cpp
@@ -6,9 +6,11 @@ using namespace std;
 
 int map[10] = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
 int p = 1;
+int masive [4] = {1,2,2,4};
 
 void swap ();
 int add3(int );
+void masiveCheck (int masive[], int n);
 
 int main(){
 
@@ -29,6 +31,11 @@ int main(){
         cout<<map[i]<<" ";
          }
          cout<<endl;
+         
+         
+         masiveCheck(masive, 4);
+         
+         
 //tasks 2; 
 
 int array [8];
@@ -46,12 +53,25 @@ int array [8];
     return 0;
 }
 
+//task
+
+
 void swap() {
     //swap values;
         int map[10] = {0, 0, 1, 1, 0, 1, 0, 0, 1, 1};
 }
 
-int add3(int ){
+int add3(int ) {
     p = p + 3;
     return p;
+}
+
+void masiveCheck(int masive[], int n) {
+	for(int i = 0; i < 4; ++i){
+		if (masive[i] + masive[i+1] == masive[i+2] + masive[i+3]){
+		cout<<"sum of both sides of masive is equal"<<endl;
+		break;
+	} cout<<"sum of both sides  of masive is !="<<endl;
+	break;
+        }
 }
