a71442c8a52870d3d9e1941dc03fe7d2b8923570
ufesa <theufesa@gmail.com>
2021-05-26 18:01:52 +0300

    added file for lesson_5

---
+++ lesson_5.cpp
@@ -0,0 +1,85 @@
+#include<iostream>
+#include<cstring>
+#include<cstdlib>
+
+using namespace std;
+
+int *map[10] = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+
+void swap ();
+
+int main(){
+
+//task 1;#include<iostream>
+#include<cstring>
+#include<cstdlib>
+
+using namespace std;
+
+int map[10] = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+
+void swap ();
+
+int main(){
+
+//task 1;
+
+    //show map;
+    
+    for (int i = 0; i < 9; i++ ) {
+        cout<<map[i]<<" ";
+    }
+    cout<<endl;
+    
+    cout<<"Call function to swap map"<<"\n";
+    
+    swap();
+    
+         for (int i = 0; i < 9; i++) {
+        cout<<map[i]<<" ";
+         }
+//tasks 2; 
+    
+
+//task 4;
+
+//Set massive 2x2
+
+//task 5;
+
+    return 0;
+}
+
+void swap() {
+    //swap values;
+        int map[10] = {0, 0, 1, 1, 0, 1, 0, 0, 1, 1};
+}
+
+    //show map;
+    
+    for (int i = 0; i < 9; i++ ) {
+        cout<<map[i]<<" ";
+    }
+    // swap();
+
+
+//tasks 3; 
+    
+
+//task 4;
+
+//Set massive 2x2
+
+//task 5;
+
+    return 0;
+}
+
+void swap() {
+    //swap values;
+        map[10] = {0, 0, 1, 1, 0, 1, 0, 0, 1, 1};
+
+     for (int i = 0; i < 9; p++) {
+        cout<<map[i]<<" ";
+    }
+}
